a6cc28c566aa27f9fbb4d500b3003041c0ba5b27
xiejinglei <2301089907@qq.com>
2021-03-09 11:21:18 +0800

    add notebook 6.2

--- orca/tf2/run-tf2-notebook.sh
+++ orca/tf2/run-tf2-notebook.sh
@@ -11,6 +11,7 @@ chmod +x ${ZOO_TUTORIALS}/orca/tf2/ipynb2py.sh
 
 set -e
 
+
 echo "#1 2.1-a-first-look-at-a-neural-network.ipynb"
 #timer
 start=$(date "+%s")
@@ -34,9 +35,37 @@ ${SPARK_HOME}/bin/spark-submit \
 now=$(date "+%s")
 time1=$((now-start))
 rm ${ZOO_TUTORIALS}/orca/tf2/tmp_test.py
-echo "#1 2.1-a-first-look-at-a-neural-network.ipynb"
+echo "#1 2.1-a-first-look-at-a-neural-network.ipynb used: $time1 seconds"
 
 
-echo "#1 2.1-a-first-look-at-a-neural-network used:$time1 seconds"
+echo "#2 6.2-understanding-recurrent-neural-networks"
+#timer
+start=$(date "+%s")
+${ZOO_TUTORIALS}/orca/tf2/ipynb2py.sh ${ZOO_TUTORIALS}/orca/tf2/6.2-understanding-recurrent-neural-networks
+sed "s/max_epoch = 10/max_epoch = 2/g" ${ZOO_TUTORIALS}/orca/tf2/6.2-understanding-recurrent-neural-networks.py > ${ZOO_TUTORIALS}/orca/tf2/tmp_test.py
+sed -i "s/plt.show()/#/g" ${ZOO_TUTORIALS}/orca/tf2/tmp_test.py    # showing the plot may stuck the test
+
+${SPARK_HOME}/bin/spark-submit \
+        --master ${MASTER} \
+        --driver-cores 2  \
+        --driver-memory 12g  \
+        --total-executor-cores 2  \
+        --executor-cores 2  \
+        --executor-memory 12g \
+        --conf spark.akka.frameSize=64 \
+        --py-files ${ANALYTICS_ZOO_PYZIP},${ZOO_TUTORIALS}/orca/tf2/tmp_test.py \
+        --properties-file ${ANALYTICS_ZOO_CONF} \
+        --jars ${ANALYTICS_ZOO_JAR} \
+        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        ${ZOO_TUTORIALS}/orca/tf2/tmp_test.py
+now=$(date "+%s")
+time2=$((now-start))
+rm ${ZOO_TUTORIALS}/orca/tf2/tmp_test.py
+echo "#2 6.2-understanding-recurrent-neural-networks used: $time2 seconds"
+
+echo "Summary:"
+echo "#1 2.1-a-first-look-at-a-neural-network used: $time1 seconds"
+echo "#2 6.2-understanding-recurrent-neural-networks used: $time2 seconds"
 
 
